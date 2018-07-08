6c5e50ee706bd9eee2d8d92740374a7c5c349015
Allan Callaghan <calaldees@gmail.com>
2018-07-08 10:06:13 +0100

    experiment with raw video - but ffmpeg dose not produce frames of the expect size in bytes. This is really beggining to frustrate me ... damn you ffmpeg!

--- mediaTimelineRenderer/renderer_video.py
+++ mediaTimelineRenderer/renderer_video.py
@@ -9,48 +9,33 @@ from .hachoir import hachoir_metadata
 
 log = logging.getLogger(__name__)
 
+
 def render_video_to_image(filename, timeline_image):
     PIXELS_PER_SECOND = 8
-    BINARY_STREAM_IMAGE_HEADER = b'\xff\xd8\xff\xe0'  # \x00\x10JFIF
 
     PATH_HOST_MEDIA = os.getenv('PATH_HOST_MEDIA')  # TODO: replace this as part of kwargs?
 
+    metadata = hachoir_metadata(filename)
+    width = metadata.get('width')
+    height = metadata.get('height')
+    timeline_image_frame_width = int((width/height) * timeline_image.height)
+    frame_size_bytes = timeline_image_frame_width * timeline_image.height * 3  # '3' is RGB bytes per pixel
+
     rate = 1/(timeline_image.height/PIXELS_PER_SECOND)
-    command_ffmpeg = f'docker run --rm -it -v {PATH_HOST_MEDIA}:{PATH_HOST_MEDIA}:ro jrottenberg/ffmpeg'
-    command_ffmpeg_pipe = f'{command_ffmpeg} -loglevel quiet -i "{os.path.join(PATH_HOST_MEDIA, filename)}" -r {rate} -f image2pipe -'
+    command_ffmpeg = f"""docker run --rm -it -v {PATH_HOST_MEDIA}:{PATH_HOST_MEDIA}:ro jrottenberg/ffmpeg -loglevel quiet"""
+    command_ffmpeg_pipe = f"""{command_ffmpeg} -i "{os.path.join(PATH_HOST_MEDIA, filename)}" -filter:v scale={timeline_image_frame_width}:{timeline_image.height} -r {rate} -vcodec rawvideo -pix_fmt rgb24 -f image2pipe -"""
+# -vframes 1
 
-    def add_image_to_timeline(image_buffer, image_sequence_number):
-        if image_buffer.tell() == 0:
-            return
-        image_buffer.seek(0)
-        try:
-            i = Image.open(image_buffer)
-            # Resize the image to add to our timeline image
-            aspect_ratio = i.width/i.height
-            #i.resize((int(i.width * aspect_ratio * timeline_image.height), timeline_image.height))
-            i.thumbnail((int(i.width * aspect_ratio * timeline_image.height), timeline_image.height), Image.ANTIALIAS)
-            print(i)
-            # Compose image on timeline image
-            timeline_image.paste(i, (image_sequence_number * timeline_image.height, 0))
-        except Exception as e:
-            log.warn('image loading error')
-            return
+    def image_generator():
+        with subprocess.Popen(command_ffmpeg_pipe, stdout=subprocess.PIPE, bufsize=2**16, shell=True) as pipe:
+            while True:
+                image_data = pipe.stdout.read(frame_size_bytes)
+                if not image_data or len(image_data) != frame_size_bytes: # this was a sign, the frame sizez were not even
+                    break
+                #print(len(image_data)) # the byte length dont match for -vframes 1
+                #print(frame_size_bytes)
+                yield Image.frombytes('RGB', (timeline_image_frame_width, timeline_image.height), image_data, 'raw')  # timeline_image.height
 
-    BUFFER_SIZE = 2**16
-    image_sequence_number = 0
-    image_buffer = BytesIO()
-    with subprocess.Popen(command_ffmpeg_pipe, stdout=subprocess.PIPE, bufsize=BUFFER_SIZE, shell=True) as pipe:
-        while True:
-            data = pipe.stdout.read(BUFFER_SIZE)
-            if not data:
-                break
-            data_split = data.split(BINARY_STREAM_IMAGE_HEADER)
-            image_buffer.write(data_split[0])
-            for data_split_chunk in data_split[1:]:
-                add_image_to_timeline(image_buffer, image_sequence_number)
-                image_sequence_number += 1
-                image_buffer.seek(0)
-                image_buffer.truncate()
-                image_buffer.write(BINARY_STREAM_IMAGE_HEADER)
-                image_buffer.write(data_split_chunk)
-    add_image_to_timeline(image_buffer, image_sequence_number)
+    for index, image in enumerate(image_generator()):
+        print(image)
+        timeline_image.paste(image, (index * timeline_image.height, 0))
